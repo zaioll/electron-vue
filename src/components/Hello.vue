1e21c25da8d5bc54efa2830babc344cb976c32ca
zaioll <zaioll@protonmail.com>
2020-12-30 01:31:09 -0400

    Código inicial

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>This is the Hello component</p>
+        <button @click="onClickGo">Go</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello',
+    methods: {
+      onClickGo() {
+        this.$router.push('/hello2')
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+    p {
+        color: deeppink;
+    }
+</style>
